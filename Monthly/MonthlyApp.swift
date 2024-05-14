dc12b90bbb84e7f49e788f18d15319622eac54a3
alcode111 <al.code.111@gmail.com>
2024-05-14 12:44:12 +0200

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by admin on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
