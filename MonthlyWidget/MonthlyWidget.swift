4a58e24a1f7ec161da08adfca827e72be29d1521
alcode111 <al.code.111@gmail.com>
2024-05-18 18:25:03 +0200

    Add Widget Extension to project, rename SimpleEntry struct change

---
+++ MonthlyWidget/MonthlyWidget.swift
@@ -0,0 +1,86 @@
+//
+//  MonthlyWidget.swift
+//  MonthlyWidget
+//
+//  Created by Ismaïl on 18/05/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> DayEntry {
+        DayEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (DayEntry) -> ()) {
+        let entry = DayEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [DayEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = DayEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct DayEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct MonthlyWidgetEntryView : View {
+    var entry: DayEntry
+
+    var body: some View {
+        ZStack {
+            ContainerRelativeShape()
+                .fill(.gray.gradient)
+                        
+            VStack {
+                HStack {
+                    Text("⛄️")
+                    
+                    Text(entry.date, style: .time)
+                }
+            }
+        }
+    }
+}
+
+struct MonthlyWidget: Widget {
+    let kind: String = "MonthlyWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                MonthlyWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                MonthlyWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .contentMarginsDisabled()
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    MonthlyWidget()
+} timeline: {
+    DayEntry(date: .now, emoji: "😀")
+    DayEntry(date: .now, emoji: "🤩")
+}
---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Ismaïl on 18/05/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
