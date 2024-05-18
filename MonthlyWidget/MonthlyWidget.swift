4c12879eba0a5a272e67b6084ae8ab2991074c7e
alcode111 <al.code.111@gmail.com>
2024-05-18 23:46:32 +0200

    Update widget to iOS17, add animations

--- MonthlyWidget/MonthlyWidget.swift
+++ MonthlyWidget/MonthlyWidget.swift
@@ -12,15 +12,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> DayEntry {
         DayEntry(date: Date())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (DayEntry) -> ()) {
         let entry = DayEntry(date: Date())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [DayEntry] = []
-
+        
         // Generate a timeline consisting of seven entries a day apart, starting from the current date.
         let currentDate = Date()
         for dayOffset in 0 ..< 7 {
@@ -29,7 +29,7 @@ struct Provider: TimelineProvider {
             let entry = DayEntry(date: startOfDate)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -40,6 +40,8 @@ struct DayEntry: TimelineEntry {
 }
 
 struct MonthlyWidgetEntryView : View {
+    @Environment(\.showsWidgetContainerBackground) var showsBackground
+    
     var entry: DayEntry
     var config: MonthConfig
     
@@ -47,72 +49,60 @@ struct MonthlyWidgetEntryView : View {
         self.entry = entry
         self.config = MonthConfig.determineConfig(from: entry.date)
     }
-
+    
     var body: some View {
-        ZStack {
+        VStack {
+            HStack(spacing: 4) {
+                Text(config.emojiText)
+                    .font(.title)
+                
+                Text(entry.date.weekdayDisplayFormat)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .minimumScaleFactor(0.6)
+                    .foregroundStyle(showsBackground ? config.weekdayTextColor : .white)
+                
+                Spacer()
+            }
+            .id(entry.date)
+            .transition(.push(from: .trailing))
+            .animation(.bouncy, value: entry.date)
+            
+            Text(entry.date.dayDisplayFormat)
+                .font(.system(size: 80, weight: .heavy))
+                .foregroundStyle(showsBackground ? config.dayTextColor : .white)
+                .contentTransition(.numericText())
+        }
+        .containerBackground(for: .widget) {
             ContainerRelativeShape()
                 .fill(config.backgroundColor.gradient)
-                        
-            VStack {
-                HStack(spacing: 4) {
-                    Text(config.emojiText)
-                        .font(.title)
-                        .containerBackground(for: .widget) {}
-                    
-                    Text(entry.date.weekdayDisplayFormat)
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .minimumScaleFactor(0.6)
-                        .foregroundStyle(config.weekdayTextColor)
-                        .containerBackground(for: .widget) {}
-                    
-                    Spacer()
-                }
-                
-                Text(entry.date.dayDisplayFormat)
-                    .font(.system(size: 80, weight: .heavy))
-                    .foregroundStyle(config.dayTextColor)
-            }
-            .padding()
         }
     }
 }
 
 struct MonthlyWidget: Widget {
     let kind: String = "MonthlyWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            if #available(iOS 17.0, *) {
-                MonthlyWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                MonthlyWidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
+            MonthlyWidgetEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
+            
         }
-        .contentMarginsDisabled()
         .configurationDisplayName("Monthly Style Widget:")
         .description("The theme of the widget changes based on month.")
         .supportedFamilies([.systemSmall])
+        .disfavoredLocations([.standBy], for: [.systemSmall])
     }
 }
 
-struct MonthlyWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        MonthlyWidgetEntryView(entry: DayEntry(date: dateToDisplay(month: 6, day: 2)))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
-
-    static func dateToDisplay(month: Int, day: Int) -> Date {
-        let components = DateComponents(calendar: Calendar.current,
-                                        year: 2024,
-                                        month: month,
-                                        day: day)
-
-        return Calendar.current.date(from: components)!
-    }
+#Preview(as: .systemSmall) {
+    MonthlyWidget()
+} timeline: {
+    MockData.dayOne
+    MockData.dayTwo
+    MockData.dayThree
+    MockData.dayFour
 }
 
 extension Date {
@@ -124,5 +114,22 @@ extension Date {
 extension Date {
     var dayDisplayFormat: String {
         self.formatted(.dateTime.day())
+    }
+}
+
+struct MockData {
+    static let dayOne = DayEntry(date: dateToDisplay(month: 11, day: 4))
+    static let dayTwo = DayEntry(date: dateToDisplay(month: 11, day: 5))
+    static let dayThree = DayEntry(date: dateToDisplay(month: 11, day: 6))
+    static let dayFour = DayEntry(date: dateToDisplay(month: 11, day: 7))
+
+
+    static func dateToDisplay(month: Int, day: Int) -> Date {
+        let components = DateComponents(calendar: Calendar.current,
+                                        year: 2022,
+                                        month: month,
+                                        day: day)
+
+        return Calendar.current.date(from: components)!
     }
 }
